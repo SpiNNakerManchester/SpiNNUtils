d57e76683e3dfc2aaee4456ad7c2c41573738e08
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-03-06 13:46:06 +0000

    More tests

--- unittests/test_conf_loader.py
+++ unittests/test_conf_loader.py
@@ -4,11 +4,13 @@ from spinn_utilities.conf_loader import ConfigurationLoader
 import pytest
 import os
 
+CFGFILE = "configloader.cfg"
+
 
 @pytest.fixture
 def default_config():
     with open(os.path.join(os.path.dirname(unittests.__file__),
-                           "configloader.cfg")) as the_file:
+                           CFGFILE)) as the_file:
         return the_file.read()
 
 
@@ -20,13 +22,13 @@ def mach_spec(tmpdir):
 
 
 def test_create():
-    ConfigurationLoader(unittests, "configloader.cfg")
+    ConfigurationLoader(unittests, CFGFILE)
 
 
 def test_basic_use(tmpdir, default_config):
     with tmpdir.as_cwd():
-        cl = ConfigurationLoader(unittests, "configloader.cfg")
-        f = tmpdir.join("configloader.cfg")
+        cl = ConfigurationLoader(unittests, CFGFILE)
+        f = tmpdir.join(CFGFILE)
         f.write(default_config)
         config = cl.load_config()
         assert config is not None
@@ -35,10 +37,10 @@ def test_basic_use(tmpdir, default_config):
         assert config.get("sect", "foo") == "bar"
 
 
-def test_advanced_use(tmpdir, default_config, mach_spec):
+def test_intermediate_use(tmpdir, default_config, mach_spec):
     with tmpdir.as_cwd():
-        cl = ConfigurationLoader(unittests, "configloader.cfg")
-        f = tmpdir.join("configloader.cfg")
+        cl = ConfigurationLoader(unittests, CFGFILE)
+        f = tmpdir.join(CFGFILE)
         f.write(default_config + "\n[Machine]\nmachine_spec_file=" +
                 mach_spec + "\n")
         config = cl.load_config()
@@ -50,3 +52,17 @@ def test_advanced_use(tmpdir, default_config, mach_spec):
                                              "machinename", "version"]
         assert config.get("Machine", "MachineName") == "foo"
         assert config.getint("Machine", "VeRsIoN") == 5
+
+
+def test_advanced_use(tmpdir, default_config):
+    with tmpdir.as_cwd():
+        cl = ConfigurationLoader(unittests, CFGFILE)
+        f = tmpdir.join(CFGFILE)
+        f.write(default_config + "\n[Abc]\ndef=1.25\n")
+        def parseAbc(parser):
+            f = parser.getfloat("Abc", "def")
+            parser.set("Abc", "ghi", f*3)
+            parser.remove_option("Abc", "def")
+        config = cl.load_config([("Abc", parseAbc)])
+        assert config.options("Abc") == ["ghi"]
+        assert config.getfloat("Abc", "ghi") == 3.75

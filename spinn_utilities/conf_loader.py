97134215b70bfe62523bb1a677e9f75df040c1d8
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-03-16 08:37:01 +0000

    Updated some docs and fixed "None" bug

--- spinn_utilities/conf_loader.py
+++ spinn_utilities/conf_loader.py
@@ -9,7 +9,19 @@ from spinn_utilities import log
 
 
 class ConfigurationLoader():
+    """ Utility for loading configuration files from a range of paths,\
+        including the user home directory and the current working directory
+    """
+
     def __init__(self, contextPackage, filename):
+        """
+
+        :param contextPackage:\
+            The package that contains the default configuration
+        :param filename:\
+            The name of the configuration file to be found in the paths\
+            searched (including the default package)
+        """
         self._contextPackage = contextPackage
         self._contextDir = os.path.dirname(os.path.realpath(
             contextPackage.__file__))
@@ -40,8 +52,10 @@ class ConfigurationLoader():
         self.print_message(home_cfg)
 
     def _logging_parser(self, config):
-        """Create the root logger with the given level.
-        Create filters based on logging levels"""
+        """ Create the root logger with the given level.
+
+            Create filters based on logging levels
+        """
         try:
             if config.getboolean("Logging", "instantiate"):
                 logging.basicConfig(level=0)
@@ -54,15 +68,25 @@ class ConfigurationLoader():
         return None
 
     def _machine_spec_parser(self, config):
-        """Load any cross-referenced machine specification file."""
+        """ Load any cross-referenced machine specification file.
+        """
         if not config.has_option("Machine", "machine_spec_file"):
             return None
         machine_spec_file_path = config.get("Machine", "machine_spec_file")
         config.read(machine_spec_file_path)
         return machine_spec_file_path
 
-    def load_config(self, config_parsers=[]):
+    def load_config(self, config_parsers=None):
+        """ Load the configuration
+
+        :param config_parsers:\
+            The parsers to parse the config with, as a list of\
+            (section name, parser); config will only be parsed if the\
+            section_name is found in the configuration files already loaded
+        :type config_parsers: list of (str, ConfigParser)
+        """
         config = ConfigParser.RawConfigParser()
+
         # When the config is being loaded in a special environment, do nothing
         if self._in_special_environment:
             return config
@@ -80,6 +104,7 @@ class ConfigurationLoader():
             config.readfp(f)
 
         if not found_configs:
+
             # Create a default spynnaker.cfg in the user home directory and get
             # them to update it.
             self._install_cfg()
@@ -87,7 +112,9 @@ class ConfigurationLoader():
 
         read = config.read(config_locations)
         read.append(default)
-        parsers = list(config_parsers)
+        parsers = list()
+        if config_parsers is not None:
+            parsers.append(config_parsers)
         parsers.append(("Logging", self._logging_parser))
         parsers.append(("Machine", self._machine_spec_parser))
 

24b66fdf881c4079757972e737697a529d619062
Christian <brenninc@cs.man.ac.uk>
2017-05-17 15:44:43 +0100

    Added options on where to read config file

--- spinn_utilities/conf_loader.py
+++ spinn_utilities/conf_loader.py
@@ -1,3 +1,4 @@
+import appdirs
 import shutil
 import os
 import ConfigParser
@@ -28,19 +29,20 @@ class ConfigurationLoader():
         self._contextDir = os.path.dirname(os.path.realpath(
             contextPackage.__file__))
         self._filename = filename
+        self._dotname = "." + filename
         try:
             self._in_read_the_docs = bool(strtobool(
                 os.environ.get("READTHEDOCS", "False")))
         except:
             self._in_read_the_docs = False
 
-    def print_message(self, filename):
+    def print_message(self):
         print "************************************"
         print("{} has been created.  Please edit this file and change \"None\""
               " after \"machineName\" to the hostname or IP address of your"
               " SpiNNaker board, and change \"None\" after \"version\" to the"
               " version of SpiNNaker hardware you are running on:".format(
-                  filename))
+                  self._filename))
         print "[Machine]"
         print "machineName = None"
         print "version = None"
@@ -49,7 +51,7 @@ class ConfigurationLoader():
     def _install_cfg(self):
         template_cfg = os.path.join(self._contextDir,
                                     "{}.template".format(self._filename))
-        home_cfg = os.path.expanduser("~/.{}".format(self._filename))
+        home_cfg = os.path.join(os.path.expanduser("~"), self._filename)
         shutil.copyfile(template_cfg, home_cfg)
         self.print_message(home_cfg)
 
@@ -95,9 +97,21 @@ class ConfigurationLoader():
         # as this is not needed to generate documents
         if self._in_read_the_docs:
             return config
-        default = os.path.join(self._contextDir, self._filename)
-        spynnaker_user = os.path.expanduser("~/.{}".format(self._filename))
-        config_locations = [spynnaker_user, self._filename]
+        # Search path for config files (lowest to highest priority)
+        default_cfg_file = os.path.join(self._contextDir, self._filename)
+        system_config_cfg_file = os.path.join(appdirs.site_config_dir(),
+            self._dotname)
+        user_config_cfg_file = os.path.join(appdirs.user_config_dir(),
+            self._dotname)
+        user_home_cfg_file = os.path.join(os.path.expanduser("~"),
+                                          self._dotname)
+        current_directory_cfg_file = os.path.join(os.curdir, self._filename)
+
+        # locations to read as well as default later overrides earlier
+        config_locations = [system_config_cfg_file,
+                            user_config_cfg_file,
+                            user_home_cfg_file,
+                            current_directory_cfg_file]
 
         found_configs = False
         for possible_config_file in config_locations:
@@ -105,18 +119,21 @@ class ConfigurationLoader():
                 found_configs = True
                 os.path.abspath(possible_config_file)
 
-        with open(default) as f:
-            config.readfp(f)
-
         if not found_configs:
+            print "You need to have at least one {} file located in "
+            "one of the following locations: {}".format(self.__filename,
+                                                        config_locations)
 
-            # Create a default spynnaker.cfg in the user home directory and get
+            # Create a default in the user home directory and get
             # them to update it.
             self._install_cfg()
             sys.exit(2)
 
+        with open(default_cfg_file) as f:
+            config.readfp(f)
+
         read = config.read(config_locations)
-        read.append(default)
+        read.insert(0, default_cfg_file)
         parsers = list()
         if config_parsers is not None:
             parsers.extend(config_parsers)

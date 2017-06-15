c949a6c7f64bddc0ee0546c7523b0126a7c38f70
Christian <brenninc@cs.man.ac.uk>
2017-06-15 12:12:53 +0100

    Small util to track log warnings already issued

--- spinn_utilities/testing/log_checker.py
+++ spinn_utilities/testing/log_checker.py
@@ -1,11 +1,31 @@
 def assert_logs_contains(level_name, log_records, sub_message):
     for record in log_records:
-        print record
         if record.levelname == level_name:
             if sub_message in record.msg:
                 return
+    for record in log_records:
+        print record
     msg = "\"{}\" not found in any {} logs".format(sub_message, level_name)
     raise AssertionError(msg)
+
+
+def assert_logs_contains_once(level_name, log_records, message):
+    found = False
+    for record in log_records:
+        if record.levelname == level_name:
+            if message == record.msg:
+                if found:
+                    for a_record in log_records:
+                        print a_record
+                    msg = "\"{}\" found twice in  {} logs" \
+                          "".format(message, level_name)
+                    raise AssertionError(msg)
+                found = True
+    if not found:
+        for record in log_records:
+            print record
+        msg = "\"{}\" not found in any {} logs".format(message, level_name)
+        raise AssertionError(msg)
 
 
 def assert_logs_error_contains(log_records, sub_message):
---
+++ unittests/test_logger_utils.py
@@ -0,0 +1,47 @@
+from spinn_utilities import logger_utils
+import spinn_utilities.testing.log_checker as log_checker
+import logging
+from testfixtures import LogCapture
+import unittest
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+
+
+class TestLoggerUtils(unittest.TestCase):
+
+    def tearDown(self):
+        logger_utils.reset()
+
+    def test_one(self):
+        with LogCapture() as l:
+            logger_utils.warn_once(logger, "a log warning")
+            log_checker.assert_logs_contains_once("WARNING", l.records,
+                                                  "a log warning")
+
+    def test_two(self):
+        with LogCapture() as l:
+            logger_utils.warn_once(logger, "a log warning")
+            logger_utils.warn_once(logger, "another log warning")
+            log_checker.assert_logs_contains_once("WARNING", l.records,
+                                                  "a log warning")
+            log_checker.assert_logs_contains_once("WARNING", l.records,
+                                                  "another log warning")
+
+
+    def test_twice(self):
+        with LogCapture() as l:
+            logger_utils.warn_once(logger, "a log warning")
+            logger_utils.warn_once(logger, "a log warning")
+            log_checker.assert_logs_contains_once("WARNING", l.records,
+                                                  "a log warning")
+
+
+    def test_multiple(self):
+        with LogCapture() as l:
+            logger_utils.warn_once(logger, "a log warning")
+            logger_utils.warn_once(logger, "another log warning")
+            logger_utils.warn_once(logger, "a log warning")
+            log_checker.assert_logs_contains_once("WARNING", l.records,
+                                                  "a log warning")

ad64b49fecb09c988e95275a9fc6ae40cf375219
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-10-29 15:21:22 +0000

    added start and end to hex

--- spinn_utilities/bytestring_utils.py
+++ spinn_utilities/bytestring_utils.py
@@ -1,19 +1,27 @@
-def as_string(bytestring):
+import six
+
+
+def as_string(bytestring, start=None, end=None):
     """
-    Retruns the length an dthe hexs values
+    Returns the length and the hexs values.
+
+    The length is always the full length irespective of the start and end.
 
     :param bytestring: data as a byteString
+    :param start: the inclusive start of the slice to return. May be None
+    :param end: the exclusive end of the slice to return. May be None
     :return: The length of the bytesting and the hex valies comma seperated
     """
-    return "(" + str(len(bytestring)) + ")" + as_hex(bytestring)
+    return "(" + str(len(bytestring)) + ")" + as_hex(bytestring, start, end)
 
 
-def as_hex(bytestring):
+def as_hex(bytestring, start=None, end=None):
     """
     Returns the bytestring as String showing the hex values
 
     :param bytestring: data as a byteString
+    :param start: the inclusive start of the slice to return. May be None
+    :param end: the exclusive end of the slice to return. May be None
     :return: Comma seperated hex values
     """
-
-    return ','.join('{:02x}'.format(x) for x in bytestring)
+    return ','.join('%02x' % i for i in six.iterbytes(bytestring[start:end]))
--- unittests/test_bytestring_utils.py
+++ unittests/test_bytestring_utils.py
@@ -11,3 +11,21 @@ def test_string():
     raw = b'helloworld'
     hex = as_string(raw)
     assert '(10)68,65,6c,6c,6f,77,6f,72,6c,64' == hex
+
+
+def test_start():
+    raw = b'helloworld'
+    hex = as_string(raw, start=2)
+    assert '(10)6c,6c,6f,77,6f,72,6c,64' == hex
+
+
+def test_end():
+    raw = b'helloworld'
+    hex = as_string(raw, end=4)
+    assert '(10)68,65,6c,6c' == hex
+
+
+def test_both():
+    raw = b'helloworld'
+    hex = as_string(raw, start=3, end=6)
+    assert '(10)6c,6f,77' == hex

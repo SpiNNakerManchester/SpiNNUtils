3e598c8de09538f839e33c92aa139bc4c24112a0
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-11-05 10:38:15 +0000

    Fix typos in docstrings

--- spinn_utilities/bytestring_utils.py
+++ spinn_utilities/bytestring_utils.py
@@ -3,25 +3,25 @@ import six
 
 def as_string(bytestring, start=None, end=None):
     """
-    Returns the length and the hexs values.
+    Returns the length and the hex values.
 
-    The length is always the full length irespective of the start and end.
+    The length is always the full length irrespective of the start and end.
 
-    :param bytestring: data as a byteString
+    :param bytestring: data as a bytestring
     :param start: the inclusive start of the slice to return. May be None
     :param end: the exclusive end of the slice to return. May be None
-    :return: The length of the bytesting and the hex valies comma seperated
+    :return: The length of the bytesting and the hex values, comma separated
     """
     return "(" + str(len(bytestring)) + ")" + as_hex(bytestring, start, end)
 
 
 def as_hex(bytestring, start=None, end=None):
     """
-    Returns the bytestring as String showing the hex values
+    Returns the bytestring as string showing the hex values
 
     :param bytestring: data as a byteString
     :param start: the inclusive start of the slice to return. May be None
     :param end: the exclusive end of the slice to return. May be None
-    :return: Comma seperated hex values
+    :return: Comma separated hex values
     """
     return ','.join('%02x' % i for i in six.iterbytes(bytestring[start:end]))

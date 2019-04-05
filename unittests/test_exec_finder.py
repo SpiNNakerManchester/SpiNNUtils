9c78ac4d0f53226816b5f4cbec3ab9fa07a6e84b
alan-stokes <alan.barry.stokes@gmail.com>
2019-04-05 15:42:45 +0100

    hopefully fix tests

--- unittests/test_exec_finder.py
+++ unittests/test_exec_finder.py
@@ -1,69 +1,63 @@
 from spinn_utilities.executable_finder import ExecutableFinder
-import unittest
-import tempfile
-import os
 
 
-class TestExecFinder(unittest.TestCase):
+def test_create_and_config(tmpdir):
+    a = tmpdir.mkdir("a")
+    b = tmpdir.mkdir("b")
+    ef = ExecutableFinder([str(a), str(b)])
+    assert ef.binary_paths == "{} : {}".format(a, b)
+    c = tmpdir.mkdir("c")
+    ef.add_path(str(c))
+    assert ef.binary_paths == "{} : {} : {}".format(a, b, c)
 
-    def test_create_and_config(self):
-        a = tempfile.mkdtemp("a")
-        b = tempfile.mkdtemp("b")
-        ef = ExecutableFinder([str(a), str(b)])
-        assert ef.binary_paths == "{} : {}".format(a, b)
-        c = tempfile.mkdtemp("c")
-        ef.add_path(str(c))
-        assert ef.binary_paths == "{} : {} : {}".format(a, b, c)
 
-    def test_find_in_no_places(self):
-        ef = ExecutableFinder([])
-        with self.assertRaises(Exception):
-            ef.get_executable_path("abc.aplx")
+def test_find_in_no_places():
+    ef = ExecutableFinder([])
+    try:
+        ef.get_executable_path("abc.aplx")
+        return False
+    except Exception:
+        return True
 
-    def test_find_in_one_place(self):
-        tmp_dir = tempfile.mkdtemp("b")
-        ef = ExecutableFinder([str(tmp_dir)])
-        w_file = tmp_dir.join("abc.aplx")
-        w = open(w_file)
-        w.write("any old content")
-        w.close()
-        assert ef.get_executable_path("abc.aplx") == str(w)
 
-    def test_find_in_two_places(self):
-        a = tempfile.mkdtemp("a")
-        b = tempfile.mkdtemp("b")
-        ef = ExecutableFinder([str(a), str(b)])
-        w_file = a.join("abc.aplx")
-        w1 = open(w_file)
-        w1.write("any old content")
-        w1.close()
-        w2_file = b.join("abc.aplx")
-        w2 = open(w2_file)
-        w2.write("any old content")
-        w2.close()
-        assert ef.get_executable_path("abc.aplx") == str(w1)
-        os.remove(w_file)
-        assert ef.get_executable_path("abc.aplx") == str(w2)
-        w1 = open(w_file)
-        w1.write("any old content")
-        assert ef.get_executable_path("abc.aplx") == str(w1)
-        os.remove(w2_file)
-        assert ef.get_executable_path("abc.aplx") == str(w1)
-        os.remove(w_file)
-        with self.assertRaises(Exception):
-            ef.get_executable_path("abc.aplx")
 
-    def test_find_no_duplicates(self):
-        a = tempfile.mkdtemp("a")
-        b = tempfile.mkdtemp("b")
-        ef = ExecutableFinder([str(a), str(b)])
-        assert ef.binary_paths == "{} : {}".format(a, b)
-        ef.add_path(str(a))
-        ef.add_path(str(a))
-        ef.add_path(str(b))
-        ef.add_path(str(b))
-        ef.add_path(str(a))
-        ef.add_path(str(a))
-        ef.add_path(str(b))
-        ef.add_path(str(b))
-        assert ef.binary_paths == "{} : {}".format(a, b)
+def test_find_in_one_place(tmpdir):
+    ef = ExecutableFinder([str(tmpdir)])
+    w = tmpdir.join("abc.aplx")
+    w.write("any old content")
+    assert ef.get_executable_path("abc.aplx") == str(w)
+
+
+def test_find_in_two_places(tmpdir):
+    a = tmpdir.mkdir("a")
+    b = tmpdir.mkdir("b")
+    ef = ExecutableFinder([str(a), str(b)])
+    w1 = tmpdir.join("a/abc.aplx")
+    w1.write("any old content")
+    w2 = tmpdir.join("b/abc.aplx")
+    w2.write("any old content")
+    assert ef.get_executable_path("abc.aplx") == str(w1)
+    w1.remove()
+    assert ef.get_executable_path("abc.aplx") == str(w2)
+    w1.write("any old content")
+    assert ef.get_executable_path("abc.aplx") == str(w1)
+    w2.remove()
+    assert ef.get_executable_path("abc.aplx") == str(w1)
+    w1.remove()
+    assert ef.get_executable_path("abc.aplx") is None
+
+
+def test_find_no_duplicates(tmpdir):
+    a = tmpdir.mkdir("a")
+    b = tmpdir.mkdir("b")
+    ef = ExecutableFinder([str(a), str(b)])
+    assert ef.binary_paths == "{} : {}".format(a, b)
+    ef.add_path(str(a))
+    ef.add_path(str(a))
+    ef.add_path(str(b))
+    ef.add_path(str(b))
+    ef.add_path(str(a))
+    ef.add_path(str(a))
+    ef.add_path(str(b))
+    ef.add_path(str(b))
+    assert ef.binary_paths == "{} : {}".format(a, b)

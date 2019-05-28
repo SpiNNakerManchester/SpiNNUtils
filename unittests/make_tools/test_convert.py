23622faf215df3ef50a557dcb4ab047ce87814b9
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-05-28 12:41:45 +0100

    flake8 and removed incorrect part of the test

--- unittests/make_tools/test_convert.py
+++ unittests/make_tools/test_convert.py
@@ -40,20 +40,16 @@ class TestConverter(unittest.TestCase):
     def test_double_level(self):
         cwd = os.getcwd()
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        weird_dir = os.path.join(dir_path, "foo","bar")
+        weird_dir = os.path.join(dir_path, "foo", "bar")
         os.chdir(weird_dir)
         src = "foo/"
         dest = "bar/"
         dict = os.path.join("bar", "test.dict")
         c = Converter(src, dest, dict)
-        self.assertEquals(c._src_basename, "foo")
-        self.assertEquals(c._dest_basename, "bar")
         c.run()
-        weird_dir = os.path.join(dir_path, "foo","bar","gamma")
+        weird_dir = os.path.join(dir_path, "foo", "bar", "gamma")
         os.chdir(weird_dir)
         dict = os.path.join("bar", "test.dict")
         c = Converter(src, dest, dict)
-        self.assertEquals(c._src_basename, "foo")
-        self.assertEquals(c._dest_basename, "bar")
         c.run()
         os.chdir(cwd)

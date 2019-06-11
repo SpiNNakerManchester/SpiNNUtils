1374dd189cb7d716775d4f9e7a5a3f515fdc24d6
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-06-11 07:56:34 +0100

    Simplify further

--- spinn_utilities/citation/__init__.py
+++ spinn_utilities/citation/__init__.py
@@ -1,6 +1,4 @@
 from .citation_updater_and_doi_generator import CitationUpdaterAndDoiGenerator
 from .citation_aggregator import CitationAggregator
-from .generate_aggregate import generate_aggregate
 
-__all__ = ["CitationUpdaterAndDoiGenerator", "CitationAggregator",
-           "generate_aggregate"]
+__all__ = ["CitationUpdaterAndDoiGenerator", "CitationAggregator"]

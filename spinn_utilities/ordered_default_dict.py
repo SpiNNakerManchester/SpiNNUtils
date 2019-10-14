abc83bb4b80da309bdee64fbb6f8ca9f26a37b96
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-10-14 11:45:03 +0100

    fixed incorrect import from collections.abc

--- spinn_utilities/ordered_default_dict.py
+++ spinn_utilities/ordered_default_dict.py
@@ -13,10 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import OrderedDict
 try:
-    from collections.abc import OrderedDict, Callable
+    from collections.abc import Callable
 except ImportError:
-    from collections import OrderedDict, Callable
+    from collections import Callable
 
 
 class DefaultOrderedDict(OrderedDict):

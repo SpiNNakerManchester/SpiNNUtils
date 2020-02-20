e6e0a84296f6174a70986ba894d7909ef4e3cd60
alan-stokes <alan.barry.stokes@gmail.com>
2020-02-20 11:57:12 +0000

    pylint

--- spinn_utilities/timer.py
+++ spinn_utilities/timer.py
@@ -14,13 +14,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
 
-# pylint: disable=no-name-in-module
+
 if sys.version_info >= (3, 7):
     # acquire the most accurate measurement available (monotonic_ns)
-    from time import monotonic_ns as now
+    from time import monotonic_ns as now # pylint: disable=no-name-in-module
 
     # have to convert to a timedelta for rest of code to read
-    from future.backports.datetime import timedelta
+    from future.backports.datetime import timedelta # pylint: disable=no-name-in-module
 
     # conversion factor
     NANO_TO_MICRO = 1000.0
@@ -31,10 +31,10 @@ if sys.version_info >= (3, 7):
         return timedelta(microseconds=time_diff / NANO_TO_MICRO)
 elif sys.version_info >= (3, 3):
     # acquire the most accurate measurement available (monotonic)
-    from time import monotonic as now
+    from time import monotonic as now # pylint: disable=no-name-in-module
 
     # have to convert to a timedelta for rest of code to read
-    from future.backports.datetime import timedelta
+    from future.backports.datetime import timedelta # pylint: disable=no-name-in-module
 
     # as montonic is fractional seconds, put into correct time delta
     def convert_to_timedelta(time_diff):

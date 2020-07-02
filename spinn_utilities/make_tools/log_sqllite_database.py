a5112044215ff69681584e4af5b144106adb4568
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-07-02 15:02:41 +0100

    flake8

--- spinn_utilities/make_tools/log_sqllite_database.py
+++ spinn_utilities/make_tools/log_sqllite_database.py
@@ -54,7 +54,7 @@ class LogSqlLiteDatabase(object):
         if spin_dirs is None:
             if os.environ.get(
                     'CONTINUOUS_INTEGRATION', 'false').lower() == 'true':
-                spin_dirs= tempfile.TemporaryDirectory()
+                spin_dirs = tempfile.TemporaryDirectory()
             else:
                 raise Exception("Environment variable SPINN_DIRS MUST be set")
         if not os.path.exists(spin_dirs):

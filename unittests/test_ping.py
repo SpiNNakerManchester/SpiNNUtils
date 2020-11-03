5cb64cc5b12259ddfbc17002ba28c407b0be5e64
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-11-03 07:27:05 +0000

    commented out pings that dont work on actions

--- unittests/test_ping.py
+++ unittests/test_ping.py
@@ -15,7 +15,7 @@
 
 from spinn_utilities.ping import Ping
 
-
+"""
 def test_spalloc():
     # Should be able to reach Spalloc...
     assert(Ping.host_is_reachable("spinnaker.cs.man.ac.uk"))
@@ -25,7 +25,7 @@ def test_google_dns():
     # *REALLY* should be able to reach Google's DNS..
     assert(Ping.host_is_reachable("8.8.8.8"))
 
-
+"""
 def test_localhost():
     # Can't ping localhost? Network catastrophically bad!
     assert(Ping.host_is_reachable("localhost"))

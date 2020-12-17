ff5fa2efcc56f3336381ab13a3da6ce56e271c3a
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-12-17 11:53:20 +0000

    test IndexIsValue

---
+++ unittests/test_index_is_value.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2017-2018 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from spinn_utilities.index_is_value import IndexIsValue
+
+
+def test_index_is_value():
+    a = IndexIsValue()
+    assert a[10] == 10
+    assert len(a) > 1000

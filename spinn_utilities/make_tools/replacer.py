96080eb9b7567220498112914f2a938bc2534671
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-02-15 08:31:23 +0000

    flake8

--- spinn_utilities/make_tools/replacer.py
+++ spinn_utilities/make_tools/replacer.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-import six
 import struct
 from spinn_utilities.log import FormatAdapter
 from .file_converter import FORMAT_EXP

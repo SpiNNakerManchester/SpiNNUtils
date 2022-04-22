8a8ec2eb58fc3092402519b3d11af694069eb178
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-04-22 10:44:10 +0100

    removed get_valid_components

--- spinn_utilities/helpful_functions.py
+++ spinn_utilities/helpful_functions.py
@@ -25,21 +25,6 @@ logger = FormatAdapter(logging.getLogger(__name__))
 FINISHED_FILENAME = "finished"
 
 
-def get_valid_components(module, terminator):
-    """ Get possible components, stripping the given suffix from their\
-        class names.
-
-    :param module: The module containing the classes to obtain.
-    :param str terminator:
-        Regular expression string to match the suffix. Anchoring not required.
-    :return: mapping from (shortened) name to class
-    :rtype: dict(str -> class)
-    """
-    terminator_re = re.compile(terminator + '$')
-    return {terminator_re.sub('', name): router
-            for name, router in inspect.getmembers(module, inspect.isclass)}
-
-
 def is_singleton(value):
     """ Tests whether the value is a singleton.
 

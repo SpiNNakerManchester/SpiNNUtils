25ca5e89134793820463fcfff63c7a3bcdf2e3c0
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-08-01 14:31:21 +0100

    remove bracket from assert

--- unittests/test_ping.py
+++ unittests/test_ping.py
@@ -24,20 +24,20 @@ skip_ping_tests = pytest.mark.skipif(
 @skip_ping_tests
 def test_spalloc():
     # Should be able to reach Spalloc...
-    assert(Ping.host_is_reachable("spinnaker.cs.man.ac.uk"))
+    assert Ping.host_is_reachable("spinnaker.cs.man.ac.uk")
 
 
 @skip_ping_tests
 def test_google_dns():
     # *REALLY* should be able to reach Google's DNS..
-    assert(Ping.host_is_reachable("8.8.8.8"))
+    assert Ping.host_is_reachable("8.8.8.8")
 
 
 def test_localhost():
     # Can't ping localhost? Network catastrophically bad!
-    assert(Ping.host_is_reachable("localhost"))
+    assert Ping.host_is_reachable("localhost")
 
 
 def test_ping_down_host():
     # Definitely unpingable host
-    assert(not Ping.host_is_reachable("169.254.254.254"))
+    assert not Ping.host_is_reachable("169.254.254.254")

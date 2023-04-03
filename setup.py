fbe306d6e3ce748a7179e29f09366daa0341bb8c
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-04-03 07:21:07 +0100

    comment setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,16 @@
 # limitations under the License.
 
 import distutils.dir_util
-
 from setuptools import setup
 import os
 import sys
+
+
 if __name__ == '__main__':
+    # Repeated installs assume files have not changed
+    # https://github.com/pypa/setuptools/issues/3236
     if len(sys.argv) > 0 and sys.argv[1] == 'egg_info':
+        # on the first call to setpy.py remove files left by previous install
         this_dir = os.path.dirname(os.path.abspath(__file__))
         build_dir = os.path.join(this_dir, "build")
         if os.path.isdir(build_dir):
@@ -26,5 +30,4 @@ if __name__ == '__main__':
         egg_dir = os.path.join(this_dir, "SpiNNUtilities.egg-info")
         if os.path.isdir(egg_dir):
             distutils.dir_util.remove_tree(egg_dir)
-    print(sys.argv)
     setup()

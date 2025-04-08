80ccf0fdea54531ed2c9af3b3e0b995481e2e1f5
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-04-08 13:53:00 +0100

    ignore imports via pyproject.toml

--- unittests/test_progress_bar.py
+++ unittests/test_progress_bar.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import pytest
-from testfixtures import LogCapture  # type: ignore[import]
+from testfixtures import LogCapture
 from typing import Callable, Tuple
 from spinn_utilities.config_setup import unittest_setup
 from spinn_utilities.progress_bar import (

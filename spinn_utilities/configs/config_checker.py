488b83d4e2776d6d0f4c1f33b1b71a8ca0ff69a7
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-04-17 13:04:18 +0100

    use slots

--- spinn_utilities/configs/config_checker.py
+++ spinn_utilities/configs/config_checker.py
@@ -14,9 +14,9 @@
 
 from collections import defaultdict
 import os
-from typing import Callable, Collection, Dict, List, Optional, Set
+from typing import Collection, Dict, List, Set
 
-from spinn_utilities.config_holder import get_default_cfgs, get_config_bool_or_none
+from spinn_utilities.config_holder import get_default_cfgs
 from spinn_utilities.configs.camel_case_config_parser import (
     CamelCaseConfigParser, TypedConfigParser)
 from spinn_utilities.exceptions import ConfigException
@@ -24,27 +24,32 @@ from spinn_utilities.exceptions import ConfigException
 
 class ConfigChecker(object):
 
-    #__slots__ = ["configs", "directories"]
+    __slots__ = ["_configs",
+                 "_default_cfgs",
+                 "_directories",
+                 "_file_path",
+                 "_lines",
+                 "_used_cfgs"]
 
     def __init__(self, directories: Collection[str]) -> None:
         # SpiNNGym needs check_all_used
-        self.configs = TypedConfigParser()
-        self.default_cfgs = get_default_cfgs()
-        for default_cfg in self.default_cfgs:
-            self.configs.read(default_cfg)
-        self.directories = directories
-        self.file_path = ""
-        self.list: List[str] = []
-        self.used_cfgs: Dict[str, Set[str]] = defaultdict(set)
+        self._configs = TypedConfigParser()
+        self._default_cfgs = get_default_cfgs()
+        for default_cfg in self._default_cfgs:
+            self._configs.read(default_cfg)
+        self._directories = directories
+        self._file_path = ""
+        self._lines: List[str] = []
+        self._used_cfgs: Dict[str, Set[str]] = defaultdict(set)
 
     def check(self, local_defaults: bool = True):
         if local_defaults:
-            self._find_double_defaults()
+            self._check_defaults()
         self._read_files()
         if local_defaults:
             self._check_all_used()
 
-    def check_defaults(self) -> None:
+    def _check_defaults(self) -> None:
         """
         Testing function to identify any configuration options in multiple default
         files.
@@ -53,26 +58,26 @@ class ConfigChecker(object):
             If two defaults configuration files set the same value
         """
         config1 = CamelCaseConfigParser()
-        for default in self.default_cfgs[:-1]:
+        for default in self._default_cfgs[:-1]:
             config1.read(default)
         config2 = CamelCaseConfigParser()
-        config2.read(self.default_cfgs[-1])
+        config2.read(self._default_cfgs[-1])
         for section in config2.sections():
             for option in config2.options(section):
                 if config1.has_option(section, option):
                     raise ConfigException(
-                        f"cfg:{self.default_cfgs} "
+                        f"cfg:{self._default_cfgs} "
                         f"repeats [{section}]{option}")
 
     def _read_files(self):
-        for directory in self.directories:
+        for directory in self._directories:
             for root, _, files in os.walk(directory):
                 for file_name in files:
                     if file_name.endswith(".cfg"):
-                        self.file_path = os.path.join(root, file_name)
+                        self._file_path = os.path.join(root, file_name)
                         self._check_cfg_file()
                     elif file_name.endswith(".py"):
-                        self.file_path = os.path.join(root, file_name)
+                        self._file_path = os.path.join(root, file_name)
                         self._check_python_file()
 
     def _check_cfg_file(self) -> None:
@@ -82,19 +87,19 @@ class ConfigChecker(object):
         :param cfg_path:
         :raises ConfigException: If an unexpected option is found
         """
-        if self.file_path in self.default_cfgs:
+        if self._file_path in self._default_cfgs:
             return
 
         config2 = TypedConfigParser()
-        config2.read(self.file_path)
+        config2.read(self._file_path)
         for section in config2.sections():
-            if not self.configs.has_section(section):
+            if not self._configs.has_section(section):
                 raise ConfigException(
-                    f"cfg:{self.file_path} has unexpected section [{section}]")
+                    f"cfg:{self._file_path} has unexpected section [{section}]")
             for option in config2.options(section):
-                if not self.configs.has_option(section, option):
+                if not self._configs.has_option(section, option):
                     raise ConfigException(
-                        f"cfg:{self.file_path} "
+                        f"cfg:{self._file_path} "
                         f"has unexpected options [{section}]{option}")
 
     def _check_python_file(self) -> None:
@@ -102,11 +107,12 @@ class ConfigChecker(object):
         A testing function to check that all the `get_config` calls work.
 
         :param py_path: path to file to be checked
-        :raises ConfigException: If an unexpected or uncovered `get_config` found
+        :raises ConfigException:
+            If an unexpected or uncovered `get_config` found
         """
-        with open(self.file_path, 'r', encoding="utf-8") as py_file:
-            self.lines = list(py_file)
-            for index, line in enumerate(self.lines):
+        with open(self._file_path, 'r', encoding="utf-8") as py_file:
+            self._lines = list(py_file)
+            for index, line in enumerate(self._lines):
                 if ("skip_if_cfg" in line):
                     parts = self._get_parts(line, index, "skip_if_cfg")
                     self._check_lines(parts)
@@ -136,7 +142,7 @@ class ConfigChecker(object):
     def _get_parts(self, line: str, index: int, start: str) -> List[str]:
         while ")" not in line:
             index += 1
-            line += self.lines[index]
+            line += self._lines[index]
         parts = line[line.find("(", line.find(start)) + 1:
                      line.find(")")].split(",")
         return parts
@@ -153,11 +159,11 @@ class ConfigChecker(object):
                 #print(parts)
                 return
 
-            if not self.configs.has_option(section, option):
+            if not self._configs.has_option(section, option):
                 raise ConfigException(
-                    f"{self.file_path} has unexpected {section=} {option=}")
+                    f"{self._file_path} has unexpected {section=} {option=}")
 
-            self.used_cfgs[section].add(option)
+            self._used_cfgs[section].add(option)
 
     def _check_get_report_path(self, parts) -> None:
         section = "Reports"
@@ -175,10 +181,10 @@ class ConfigChecker(object):
             elif part.startswith("n_run="):
                 pass
             else:
-                if self.file_path.endswith("config_holder.py"):
+                if self._file_path.endswith("config_holder.py"):
                     return
                 raise ConfigException(
-                    f"in {self.file_path} unexpected {parts=}")
+                    f"in {self._file_path} unexpected {parts=}")
 
         if option == "option":
             return
@@ -186,25 +192,25 @@ class ConfigChecker(object):
         option = option.replace("'", "").replace('"', '')
         section = section.replace("'", "").replace('"', '')
 
-        if not self.configs.has_option(section, option):
-            if self.file_path.endswith("config_checker.py"):
+        if not self._configs.has_option(section, option):
+            if self._file_path.endswith("config_checker.py"):
                 return
             raise ConfigException(
-                f"{self.file_path} has unexpected {section=} {option=}")
+                f"{self._file_path} has unexpected {section=} {option=}")
 
-        self.used_cfgs[section].add(option)
+        self._used_cfgs[section].add(option)
 
     def _check_all_used(self):
         current_config = TypedConfigParser()
-        current_config.read(self.default_cfgs[-1])
+        current_config.read(self._default_cfgs[-1])
         for section in current_config:
-            if section not in self.used_cfgs:
+            if section not in self._used_cfgs:
                 if section == "DEFAULT":
                     continue
                 raise ConfigException(
-                    f"in {self.default_cfgs[-1]} {section=} was never used")
+                    f"in {self._default_cfgs[-1]} {section=} was never used")
             for option in current_config.options(section):
-                if option not in self.used_cfgs[section]:
+                if option not in self._used_cfgs[section]:
                     raise ConfigException(
-                        f"in {self.default_cfgs[-1]} cfg {section=} {option=} "
+                        f"in {self._default_cfgs[-1]} cfg {section=} {option=} "
                         f"was never used")

9a686bad620493b5b0b3c9fea12b464d04308d55
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-05-16 08:06:37 +0100

    adds_typing not needed

--- spinn_utilities/log.py
+++ spinn_utilities/log.py
@@ -260,8 +260,7 @@ class FormatAdapter(logging.LoggerAdapter):
                 log_kwargs["exc_info"] = kwargs["exc_info"]
             self.do_log(level, message, (), **log_kwargs)
 
-    @overrides(logging.LoggerAdapter.process, extend_doc=False,
-               adds_typing=True)
+    @overrides(logging.LoggerAdapter.process, extend_doc=False)
     def process(self, msg: object, kwargs: Any) -> Tuple[object, dict]:
         """
         Process the logging message and keyword arguments passed in to a

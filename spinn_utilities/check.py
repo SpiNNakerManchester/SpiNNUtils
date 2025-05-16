615d543a72d28fac7f9acd430f70b0aad3e7fc2d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-05-16 10:15:06 +0100

    more checks

--- spinn_utilities/check.py
+++ spinn_utilities/check.py
@@ -17,27 +17,77 @@ from typing import List
 
 
 class Base(object):
+    """ The parent """
     def four_params(self, x: int, y: int, z: int) -> List[int]:
         """I have four params including self"""
         return [x, y, z]
 
+    def named_params(self, x: int, *, y: int, z: int) -> List[int]:
+        """I have twoe unnamed (including self) and two named params"""
+        return [x, y, z]
+
 
 class TooMany(Base):
+    """  param too many"""
     # overrides(Base.four_params)
     def four_params(self, x: int, y: int, z: int, w: int) -> List[int]:
-        """ Oops 1 param too many"""
-        return [w, z, y, x]
+        """1 param too many"""
+        return  [x, y, z, w]
+
+    def named_params(self, x: int, *, y: int, z: int, w: int) -> List[int]:
+        """1 named param too many"""
+        return [x, y, z, w]
+
+
+class ExtraDefaulted(Base):
+    """ Ok to add a param if defaulted """
+    # overrides(Base.four_params)
+    def four_params(self, x: int, y: int, z: int, w: int = 2) -> List[int]:
+        """ Ok to add a param if defaulted """
+        return [x, y, z, w]
+
+    def named_params(self, x: int, *, y: int, z: int, w: int = 2) -> List[int]:
+        """1 named param too many"""
+        return [x, y, z, w]
 
 
 class TooFew(Base):
+    """1 param missing"""
     # overrides(Base.four_params)
-    def four_params(self, x: int, y: int, z: int, w: int) -> List[int]:
-        """ Oops 1 param missing"""
-        return [w, z, y, x]
+    def four_params(self, x: int, z: int) -> List[int]:
+        """ 1 param missing"""
+        return [x, z]
+
+    def named_params(self, x: int, *, z: int) -> List[int]:
+        """ 1 param missing"""
+        return [x, z]
 
 
 class Renamed(Base):
+    """ 1 param renamed """
     # overrides(Base.four_params)
     def four_params(self, x: int, p: int, z: int) -> List[int]:
-        """ Oops 1 param renamed """
+        """ 1 param renamed """
         return [x, p, z]
+
+    def named_params(self, x: int, *, p: int, z: int) -> List[int]:
+        """ 1 param renamed """
+        return [x, p, z]
+
+
+# while if all works does not mean we like it
+too_many = TooMany()
+print(too_many.four_params(1, 2, 3, 4))
+print(too_many.named_params(1, y=2, z=3, w=4))
+
+too_many = ExtraDefaulted()
+print(too_many.four_params(1, 2, 3, 4))
+print(too_many.named_params(1, y=2, z=3, w=4))
+
+too_few = TooFew()
+print(too_few.four_params(1, 2))
+print(too_few.named_params(1, z=2))
+
+renamed = Renamed()
+print(renamed.four_params(1, 2, 3))
+print(renamed.named_params(1, p=2, z=3))

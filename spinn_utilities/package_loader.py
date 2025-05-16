c0551aa26396ac9cce57460f707ca4b8fbde51f5
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-05-16 09:19:08 +0100

    put back import os

--- spinn_utilities/package_loader.py
+++ spinn_utilities/package_loader.py
@@ -12,11 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import sys
 import traceback
 from typing import List, Optional, Set
-
-from spinn_utilities.overrides import overrides
 
 
 def all_modules(directory: str, prefix: str,

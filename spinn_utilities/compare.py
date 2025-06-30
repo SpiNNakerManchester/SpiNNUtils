851c9415500726ae0f8542b401e7515693457948
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-06-30 11:17:54 +0100

    compare doc styles

---
+++ spinn_utilities/compare.py
@@ -0,0 +1,11 @@
+class Compare(object):
+
+    def short_description(self) -> int:
+        """
+        The number 1
+        """
+
+    def doc_return(self) -> int:
+        """
+        :return: The number 1
+        """

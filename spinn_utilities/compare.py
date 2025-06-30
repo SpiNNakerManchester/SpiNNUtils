94c3fc823fb7d8a815de08db23c716b91864146a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-06-30 11:59:45 +0100

    getter and setter

--- spinn_utilities/compare.py
+++ spinn_utilities/compare.py
@@ -4,8 +4,24 @@ class Compare(object):
         """
         Sets funny odd object
         """
+        self.foo = foo
 
     def doc_param(self, foo:int) -> None:
         """
         :param foo: The funny odd object to set
         """
+        self.foo = foo
+
+    @property
+    def foo(self) -> int:
+        """
+        :return: The funny odd object - property
+        """
+        return self.foo
+
+    @foo.setter
+    def foo(self, foo: int):
+        """
+        Sets funny odd object - setter
+        """
+        self.foo = foo

4aa6aa32bc69a8d97f46dff3b1913fc7e571ae2a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2026-01-08 09:38:33 +0000

    move test to a code check

--- unittests/make_tools/test_database_keys.py
+++ unittests/make_tools/test_database_keys.py
@@ -36,37 +36,3 @@ class TestDatabaseKeys(unittest.TestCase):
             LogSqlLiteDatabase.key_from_filename("short")
         with self.assertRaises(ValueError):
             LogSqlLiteDatabase.key_from_filename("logs.sqlite3")
-
-    def test_check_all(self) -> None:
-        """
-        This test is maily intended to be run after automatic_make
-
-        It will check all parallel repositories use unque database keys
-        """
-        unittest_setup()
-        class_file = str(sys.modules[self.__module__].__file__)
-        this_path = os.path.dirname(os.path.abspath(class_file))
-        test_path = os.path.dirname(this_path)
-        utils_path = os.path.dirname(test_path)
-        all_path = os.path.dirname(utils_path)
-
-        # exclude dirs that may have an aplx but not logs database
-        excludes = set(["JavaSpiNNaker", "pyenv", "spinnaker_tools",
-                        "SpiNNakerManchester.github.io"])
-        for root, _dirs, files in os.walk(all_path):
-            for exclude in excludes:
-                if exclude in _dirs:
-                    _dirs.remove(exclude)
-            aplx_found = False
-            for file in files:
-                if file.endswith(".aplx"):
-                    aplx_found = True
-            if aplx_found:
-                UtilsDataView.register_binary_search_path(root)
-                print(root)
-        # Hack for test do not copy
-        database_map = (
-            UtilsDataView._UtilsDataView__data.  # type: ignore[attr-defined]
-            _log_database_paths)
-        for database_key, database_path in database_map.items():
-            print(database_key, database_path)
